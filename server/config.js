b3163c31a498888334c6152d8d1cde84342c2ef3
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2017-04-22 16:16:48 +0300

    Some fixes

--- server/config.js
+++ server/config.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const {
+    join
+} = require('path');
+
 module.exports = {
-	port: 9037,
-	logFile:'\\logs\\log.txt'
+    port: 9037,
+    logFile: join(__dirname, 'logs/logs.txt')
 };
--- server/middlwares/loger.js
+++ server/middlwares/loger.js
@@ -1,9 +1,19 @@
-let logFile = process.cwd()+require('../config.js').logFile;
-let fs = require('fs');
-module.exports = function(req,res,next){
-	let logData = req.url+"|"+req.method+"|"+Date.now()+"\r\n";
-	fs.appendFile(logFile,logData,(err)=>{
-		if(err){throw new Error(err);}
-	});
-	next();
-}
+'use strict';
+
+const fs = require('fs');
+
+const {
+    logFile
+} = require('../config.js');
+
+module.exports = function (req, res, next) {
+    let logData = `${req.url}|${req.method}|${Date.now()}\n`;
+
+    fs.appendFile(logFile, logData, err => {
+        if (err) {
+            throw new Error(err);
+        }
+    });
+
+    next();
+};
